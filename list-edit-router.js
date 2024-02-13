f4076424f544d7df241e972d80ddf5fa37019c7a
jose-deltoro <montesdeltorojosearmando@gmail.com>
2024-02-13 17:31:02 -0500

    corrigiendo las tareas y esperando que todo este bien

--- list-edit-router.js
+++ list-edit-router.js
@@ -1,8 +1,54 @@
 const {Router} = require("express")
 const router = Router();
 
+const cuerpoVacio = (req, res, next) => {
+
+  if (req.method === 'POST') {
+    if (Object.keys(req.body).length === 0) {
+      return res.status(400).json({ error: 'Solicitudes POST con el cuerpo vacio.' });
+    } else{const { id, isCompleted, description } = req.body;
+    if (!id || !isCompleted || !description) {
+      return res.status(400).json({ error: 'Información no valida o Faltan atributos obligatorios en la solicitud de la tarea.' });}
+  } if (req.method === 'PUT') {
+    if (!req.body || Object.keys(req.body).length === 0) {
+      return res.status(400).json({ error: 'Cuerpo de la solicitud PUT vacío' });
+    }
+  } 
+  next();
+}};
+
+const solicitudNoValida = (req, res, next) => {
+
+  if (req.method === 'POST') {
+    const { id, isCompleted, description } = req.body;
+    if (!id || !isCompleted || !description) {
+      return res.status(400).json({ error: 'Información no valida o Faltan atributos obligatorios en la solicitud de la tarea.' });
+    } else if (req.method === 'PUT') {
+      const { id, isCompleted, description } = req.body;
+      if (!id || !isCompleted || !description) {
+        return res.status(400).json({ error: 'Información no valida o Faltan atributos obligatorios en la solicitud de la tarea.' });
+      }
+    next();
+  } else {
+
+    next();
+  }
+}};
+
+Router.use(express.json()); 
+Router.use(cuerpoVacio);
+
+
+Router.post('', (req, res) => {
+  res.json({ mensaje: 'Solicitudes POST con el cuerpo vacio.' });
+});
+
 router.post('/crear-tarea', (req, res) => {
     res.send("Tarea añadida")
+})
+
+router.put('/crear-tarea', (req, res) => {
+  res.send("Tarea añadida")
 })
 
 router.delete('/eliminar-tarea/:idTarea', (req, res) => {
